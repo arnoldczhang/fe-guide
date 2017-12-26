b93bdf9b7a55364b6acb581eaca28f2036a3c83f
arnold <zhangcheng17@meituan.com>
2017-12-26 21:03:28 +0800

    1.init promise;

---
+++ src/prototype/README.md
@@ -0,0 +1,3 @@
+## 参考链接
+
+- https://zhuanlan.zhihu.com/p/22787302
--- src/prototype/prototype.js
+++ src/prototype/prototype.js
@@ -1,1 +1,59 @@
-// TODO
+(function () {
+  var $define = Object.defineProperty;
+  var store = {
+    queue: {},
+    single: {}
+  };
+
+  $define(store, '_set', {
+    enumerable: false,
+    value: function (map, key, value) {
+      store[map][key] = value;
+    }
+  });
+
+  $define(store, '_get', {
+    enumerable: false,
+    value: function (map, key) {
+      return store[map][key];
+    }
+  });
+
+  $define(store, 'setQueue', {
+    enumerable: false,
+    value: function (key, value) {
+      return store._set('queue', key, value);
+    }
+  });
+
+  $define(store, 'getQueue', {
+    enumerable: false,
+    value: function (key) {
+      return store._get('queue', key);
+    }
+  });
+
+  $define(store, 'setSingle', {
+    enumerable: false,
+    value: function (key, value) {
+      return store._set('single', key, value);
+    }
+  });
+
+  $define(store, 'getSingle', {
+    enumerable: false,
+    value: function (key) {
+      return store._get('single', key);
+    }
+  });
+
+  if (Object.freeze) {
+    Object.freeze(store);
+  }
+
+  function Promise() {
+
+  };
+
+  window.Promise = window.Promise || Promise;
+} ());
