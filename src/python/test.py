02d7055854b9085358015f42fa30289882a26e5e
arnold <zhangcheng17@meituan.com>
2018-01-31 15:12:57 +0800

    1.add multi thread； 2.create cach；

--- src/python/test.py
+++ src/python/test.py
@@ -1,4 +1,4 @@
-import requests, sys, wx, xlrd, re, json, datetime
+import requests, sys, wx, xlrd, re, json, datetime, os, xlsxwriter, threading
 from pyquery import PyQuery as pq
 from tkinter import *
 from tkinter import ttk
@@ -37,9 +37,12 @@ def config_sys_info():
     'project': '',
   }
 
-def get_now(format = '%Y-%m-%d'):
+def get_now(format = '%Y-%m-%d', need_print = False):
   now = datetime.datetime.now()
-  return now.strftime(format)
+  result = now.strftime(format)
+  if need_print:
+    print(result)
+  return result
 
 def append_str(result, new_word):
   return '%s\n%s' % (result, new_word)
@@ -61,7 +64,7 @@ class MainWindow(Frame):
     self.createWidgets()
 
   def run_test(self):
-    print(get_now())
+    print(1)
 
   def get_cach(self):
     try:
@@ -82,7 +85,8 @@ class MainWindow(Frame):
     self.error_label = Label(self, text = '错误内容：')
     self.error_label.pack()
     self.error_input = ttk.Combobox(self)
-    self.error_input.bind("<<ComboboxSelected>>", self.combo_select)  
+    self.error_input['values'] = self.storage.get('error_list') or ()
+    self.error_input.bind('<<ComboboxSelected>>', self.combo_select)  
     self.error_input.pack()
 
     self.project_label = Label(self, text = '项目名称：')
@@ -109,14 +113,14 @@ class MainWindow(Frame):
     self.start_time_label = Label(self, text = '开始时间：')
     self.start_time_label.pack()
     default_value = StringVar()
-    default_value.set('00:00')
+    default_value.set(self.storage.get('start_time') or '00:00')
     self.start_time_input = Entry(self, textvariable = default_value)
     self.start_time_input.pack()
 
     self.end_time_label = Label(self, text = '结束时间：')
     self.end_time_label.pack()
     default_value = StringVar()
-    default_value.set('23:30')
+    default_value.set(self.storage.get('end_time') or '23:30')
     self.end_time_input = Entry(self, textvariable = default_value)
     self.end_time_input.pack()
 
@@ -143,7 +147,7 @@ class MainWindow(Frame):
 
   def combo_select(self, *args):
     self.sample_input.delete(0, 'end')
-    self.sample_input.insert(0, TEMP[self.error_input.get()])
+    self.sample_input.insert(0, TEMP.get(self.error_input.get()) or '1')
 
   def select_file_dir(self):
     file_dir = filedialog.askdirectory()
@@ -177,64 +181,75 @@ class MainWindow(Frame):
       content = 'all'
 
     self.query_handler({
-      "content": content,
-      "limit": limit,
-      "file_dir": file_dir,
-      "project": project,
-      "JSESSIONID": session,
-      "ssoid": ssoid,
-      "date": date,
-      "start_time": start_time,
-      "end_time": end_time
+      'content': content,
+      'limit': limit,
+      'file_dir': file_dir,
+      'project': project,
+      'JSESSIONID': session,
+      'ssoid': ssoid,
+      'date': date,
+      'start_time': start_time,
+      'end_time': end_time
     }, {
       'error_input': self.error_input
     }, type)
 
-## crawler
+## Application
 class Application(object):
   def __init__(self, param):
     self.result = {}
+    self.cookie = {}
     self.param = param
     self.map = param['map']
-    self.createWidget()
+    self.create_widget()
 
   def query_handler(self, param, widget, log_type = TYPE['DETAIL']):
-    self.query_content = param["content"] or self.param['key']
-    self.limit = param["limit"] or self.param['limit']
-    self.file_dir = param["file_dir"] or self.param['path_name']
-    self.project = param["project"] or self.param['project']
-    self.date = param['date']
-    self.start_time = param['start_time']
-    self.end_time = param['end_time']
     self.widget = widget
     self.log_type = log_type
-    self.cookie = {
-      "JSESSIONID": param['JSESSIONID'] or '',
-      "ssoid": param['ssoid'] or ''
-    }
+    self.extend_param(param)
+    self.update_storage(param)
     self.ajax()
     if is_detail(log_type):
       self.simple_query()
       self.write_result()
 
-  def createWidget(self):
+  def extend_param(self, param = {}):
+    self.query_content = param['content'] or self.param['key']
+    self.limit = param['limit'] or self.param['limit']
+    self.file_dir = param['file_dir'] or self.param['path_name']
+    self.project = param['project'] or self.param['project']
+    self.date = param['date']
+    self.start_time = param['start_time']
+    self.end_time = param['end_time']
+
+  def update_storage(self, param = {}):
+    try:
+      self.storage
+    except:
+      self.storage = {}
+    self.cookie['JSESSIONID'] = self.storage['JSESSIONID'] = param['JSESSIONID'] or ''
+    self.cookie['ssoid'] = self.storage['ssoid'] = param['ssoid'] or ''
+    self.storage['start_time'] = param['start_time'] or ''
+    self.storage['end_time'] = param['end_time'] or ''
+
+  def create_widget(self):
     self.app = MainWindow(self.query_handler)
     self.app.master.geometry('500x550+500+200')
     self.app.master.title('监控')
     self.app.mainloop()
 
   def append_result(self, uniq_key):
+    uniq_result = self.result[uniq_key]
     def append(index, item):
       element = self.q(item).text()
-      result = self.result
-      if result[uniq_key].get(element) != None:
-        result[uniq_key][element] = result[uniq_key][element] + 1
+      if uniq_result.get(element) != None:
+        uniq_result[element] += 1
       else:
-        result[uniq_key][element] = 1
+        uniq_result[element] = 1
     return append
 
   def append_list(self, array):
-    point_re = re.compile(r",")
+    point_re = re.compile(r',')
     def append(index, item):
       children = self.q(item).children()
       error_key = self.q(children.eq(0)).text()
@@ -247,14 +262,30 @@ class Application(object):
     self.q = pq(self.response_text)
     input_values = []
     self.q('table.table-striped tr').each(self.append_list(input_values))
-    self.widget['error_input']["values"] = input_values
+    self.storage['error_list'] = input_values
+    self.widget['error_input']['values'] = input_values
     messagebox.showinfo('提示', '获取错误信息列表成功')
+
+  def run_thread(self, index, callback):
+    self.q('table tr:eq(%s) td:eq(1)' % (index)).each(callback)
 
   def simple_query(self):
     self.q = pq(self.response_text)
-    for key in self.map:
+    queue = []
+    for index,key in enumerate(self.map):
       self.result[key] = {}
-      self.q('table tr:eq(%s) td:eq(1)' % (self.map[key])).each(self.append_result(key))
+      append_result = self.append_result(key)
+      thread = threading.Thread(target=self.run_thread, args=(self.map[key], append_result,))
+      thread.start()
+      queue.append(thread)
+    for thread in queue:
+      thread.join()
+
+  def cach_storage(self):
+    f = open(CACH_FILE_NAME, 'w')
+    print(self.storage)
+    f.write(json.dumps(self.storage, indent = 4, ensure_ascii=True))
+    f.close()
 
   def ajax(self):
     url = '%s?op=%s&logQuery.day=%s&logQuery.startTime=%s&logQuery.endTime=%s&logQuery.project=%s&logQuery.pageUrl=all&logQuery.level=error&logQuery.category=jsError&logQuery.platform=-1&logQuery.city=-1&logQuery.network=-1&logQuery.operator=-1&logQuery.container=-1&logQuery.limit=%s&logQuery.offset=0&logQuery.secCategory=%s' % (HOST, self.log_type, self.date, self.start_time, self.end_time, self.project, self.limit, self.query_content)
@@ -263,29 +294,31 @@ class Application(object):
     login_match = re.compile(r'注销').search(self.response_text)
     if login_match:
       print('请求成功...')
-      f = open(CACH_FILE_NAME, 'w')
-      f.write(json.dumps(self.cookie, indent = 4, ensure_ascii=False))
-      f.close()
       if is_list(self.log_type):
         self.simple_list_query()
+      self.cach_storage()
     else:
-      print('请登录...')
       webbrowser.open_new_tab(url)
       self.browser = webbrowser.get()
       messagebox.showinfo('提示', '当前未登录，请登录完之后，回填JSESSIONID 和 ssoid')
 
   def write_result(self):
-    table = Workbook()
+    restrict_re = re.compile(r'[,\/\s\|:]')
+    query_content = restrict_re.sub('', self.query_content)
+    file_path = '%s/%s.xlsx' % (self.file_dir, query_content)
+    workbook = xlsxwriter.Workbook(file_path)
     for result_key in self.result:
       result = self.result[result_key]
-      sheet = table.add_sheet(result_key, cell_overwrite_ok = True)
-      sheet.write(0, 0, '内容')
-      sheet.write(0, 1, '出现次数')
-      for index,key in enumerate(result):
-        sheet.write(index + 1, 0, key)
-        sheet.write(index + 1, 1, result[key])
-    table.save('%s/%s.xls' % (self.file_dir, self.query_content))
+      worksheet = workbook.add_worksheet(result_key)
+      worksheet.write(0, 0, '内容')
+      worksheet.write(0, 1, '出现次数')
+      result = sorted(result.items(), key=lambda d: -d[1])
+      for index, item in enumerate(result):
+        worksheet.write(index + 1, 0, item[0])
+        worksheet.write(index + 1, 1, item[1])
+    workbook.close()
     messagebox.showinfo('提示', '保存成功')
+    os.system('open %s' % (file_path))
 
 ## run
 Application(config_sys_info())
