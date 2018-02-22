722e74143f8bb7962a6368231f73ce72e27ee891
arnold <zhangcheng17@meituan.com>
2018-02-22 18:01:19 +0800

    1.update；

--- src/js_related/my.js
+++ src/js_related/my.js
@@ -11,17 +11,32 @@ form.submit();
 document.body.removeChild(ifr);
 document.body.removeChild(form);
 
+
+
+
 // 正则转义字符
 * . ? + $ ^ [ ] ( ) { } | \ /
+
+
+
+
 
 //https证书
 openssl genrsa 1024 > ./ssl/private.pem
 openssl req -new -key ./ssl/private.pem -out csr.pem
 openssl x509 -req -days 365 -in csr.pem -signkey ./ssl/private.pem -out ./ssl/file.crt
 
+
+
+
+
 // indexOf VS search
 search:默认处理正则，比如'aaa\n'.search('.')，是0，因为/./
 indexOf:处理字符串，同是字符串，略快
+
+
+
+
 
 // 0.1 + 0.2 !== 0.3 
 // 计算机中存储小数是先转换成二进制进行存储
@@ -33,3 +48,13 @@ indexOf:处理字符串，同是字符串，略快
 
 
 
+
+// isObject
+// 类似new String('aa')，不算
+function isProxyable(value) {
+  if (!value) return false;
+  if (typeof value !== "object") return false;
+  if (Array.isArray(value)) return true;
+  const proto = Object.getPrototypeOf(value);
+  return proto === null || proto === Object.prototype;
+}
