ed26703331daa3d45ad5f33f953c7059d2a9f6e0
arnold <zhangcheng17@meituan.com>
2018-02-27 18:30:49 +0800

    1.update；

--- src/immer/immer.js
+++ src/immer/immer.js
@@ -25,9 +25,14 @@ void function __immer(global, factory) {
     },
   };
 
+  var KEY = {
+    TARGET: '[[Target]]',
+  };
+
   /**
    * utils
    */
+  var isArray = Array.isArray;
   function isFunction(func) {
     return typeof func === 'function';
   };
@@ -52,17 +57,15 @@ void function __immer(global, factory) {
     return result;
   };
 
-  function getResult(proxy, baseProxy) {
+  function getResult(proxy, base) {
     if (isUndefined(proxy)) {
-      proxy = baseProxy;
+      proxy = base;
     }
-    return proxy.target;
+    return proxy;
   };
 
-  function deepProxy(state) {
-    return new Proxy(state, {
-
-    });
+  function bindProxy(state) {
+    return new $Proxy(state, default_handler);
   };
 
   function isProxyable(state) {
@@ -73,14 +76,51 @@ void function __immer(global, factory) {
   /**
    * Proxy
    */
-  if (true || isUndefined(Proxy)) {
-    function Proxy(target, handler) {
-      this['[[Target]]'] = target;
-      this['[[Handler]]'] = handler;
-    };
-    var proxyPro = Proxy.prototype;
-  }
+  function $Proxy(target, handler) {
+    this.draft = {};
+    this[KEY.TARGET] = target;
+    this['[[Handler]]'] = handler;
+    this.proxy();
+    return this.draft;
+  };
 
+  var proxyPro = $Proxy.prototype;
+  proxyPro.proxy = function proxy(target, draft) {
+    var _this = this;
+    target = target || _this[KEY.TARGET];
+    draft = draft || _this.draft;
+    Object.keys(target).forEach(function(key) {
+      var value = target[key];
+      var objValue;
+      if (isObject(value)) {
+        value = isArray(value) || 'length' in value ? [] : {};
+        objValue = _this.proxy(target[key], value);
+        Object.defineProperty(draft, key, {
+          get: function _getter() {
+            return objValue;
+          },
+          set: function _setter(val) {
+            if (val !== objValue) {
+              target[key] = objValue = val;
+            }
+          },
+        });
+        return;
+      }
+
+      Object.defineProperty(draft, key, {
+        get: function _getter() {
+          return target[key];
+        },
+        set: function _setter(val) {
+          if (val !== target[key]) {
+            target[key] = val;
+          }
+        }
+      });
+    });
+    return draft;
+  };
 
   /**
    * immer
@@ -101,9 +141,9 @@ void function __immer(global, factory) {
               if (argsQueue.length >= argsLength) {
                 base = argsQueue[0];
                 if (isProxyable(base)) {
-                  baseState = deepProxy(base);
+                  baseState = bindProxy(base);
                   argsQueue[0] = baseState;
-                  return getResult(callback.apply(baseState, argsQueue), baseState);
+                  return getResult(callback.apply(baseState, argsQueue), base);
                 }
               }
             }
@@ -116,8 +156,8 @@ void function __immer(global, factory) {
       } else {
         base = baseState;
         if (isProxyable(base)) {
-          baseState = deepProxy(base);
-          return getResult(callback.call(baseState, baseState), baseState);
+          baseState = bindProxy(base);
+          return getResult(callback.call(baseState, baseState), base);
         }
       }
     } else {
--- src/immer/test.js
+++ src/immer/test.js
@@ -1,147 +1,154 @@
-import produce from 'immer';
+import produce from './immer';
+// import produce from 'immer';
 
-const PROXY_STATE = Symbol('PROXY_STATE');
-const states = [];
-const cach = {};
-const base = {
-  array: [1, 2, 3],
-};
-
-const producer = function() {
-  // ...
-};
-
-const markChanged = (state) => {
-  state.modified = true;
-  if (state.parent) {
-    markChanges(state.parent);
-  }
-};
-
-const markChanges = () => {
-  states.forEach((state) => {
-    if (Array.isArray(state.base)) {
-      if(!shallowEqual(state.proxy, state.base)) {
-        markChanged(state);
-      }
-    } else if (Object.keys(state.proxy).length !== Object.keys(state.base).length) {
-      markChanged(state);
-    }
-  });
-};
-
-const finalize = (proxy) => {
-  if (proxy && proxy[PROXY_STATE]) {
-    if (proxy.modified) {
-      const state = proxy[PROXY_STATE];
-      if (state.finalized) {
-        return state.copy;
-      }
-      state.finalized = true;
-      state.copy = shallowCopy(proxy);
-      const base = state.base;
-      each(state.copy, (key, value) => {
-        if (value !== base[key]) {
-          copy[key] = finalize(value);
-        }
-      });
-      Object.freeze(copy);
-    } else {
-      return state.base;
-    }
-  }
-  aa: each(proxy, (index, child) => {
-    if (proxy && proxy[PROXY_STATE]) {
-      proxy[index] = finalize(child);
-    } else {
-      aa(child);
-    }
-  });
-  return proxy;
-};
-
-const prev = states;
-
-try{
-  // shallowCopy
-  const proxy = Object.assign({}, base);
-
-  // createPropertyProxy
-  Object.keys(base).forEach((key) => {
-    Object.defineProperty(proxy, key, cach[key] || {
-      configurable: true,
-      enumerable: true,
-      get() {
-          return get(this[PROXY_STATE], key)
-      },
-      set(value) {
-          set(this[PROXY_STATE], key, value)
-      }
-    });
-  });
-
-  // createState
-  const state = {
-    modified: false,
-    hasCopy: false,
-    parent: undefined,
-    base,
-    proxy,
-    copy: undefined,
-    finished: false,
-    finalizing: false,
-    finalized: false,
-  };
-
-  // createHiddenProperty
-  Object.defineProperty(proxy, PROXY_STATE, {
-    value: state,
-    enumerable: false,
-    writable: true
-  });
-  states.push(state);
-
-  const returnValue = producer.call(proxy, proxy);
-
-  states.forEach(state => state.finalizing = true);
-
-  markChanges();
-
-  result = finalize(proxy);
-
-  if (returnValue !== undefined && returnValue !== proxy) {
-    if (proxy[PROXY_STATE].modified) {
-      throw new Error('');
-    }
-    result = returnValue;
-  }
-
-  each(states, (i, state) => {
-    state.finished = true;
-  });
-  return result;
-} finally {
-  states = prev;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-produce(state, (draft) => {
-  console.table(state);
-  console.table(draft);
+var res = produce({
+  a: {
+    b: [{b1: 1}, {b2: 2},{b3: 3}],
+  },
+}, function(draft) {
+  console.log(draft.a);
+  console.log(draft.a.b);
+  console.log(draft.a.b[0]);
+  console.log(draft.a.b[0].b1);
+  console.log(draft.a.b[1]);
+  console.log(draft.a.b[1].b2);
+  draft.a.b[1].b2 = 100;
+  console.log(draft.a.b[1].b2);
+  draft.a.b[0] = 100;
+  draft.a.b[0]
+  console.log(draft, this);
 });
+
+// res({a: 1});
+// res(1);
+// res(1);
+
+
+// const PROXY_STATE = Symbol('PROXY_STATE');
+// const states = [];
+// const cach = {};
+// const base = {
+//   array: [1, 2, 3],
+// };
+
+// const producer = function() {
+//   // ...
+// };
+
+// const markChanged = (state) => {
+//   state.modified = true;
+//   if (state.parent) {
+//     markChanges(state.parent);
+//   }
+// };
+
+// const markChanges = () => {
+//   states.forEach((state) => {
+//     if (Array.isArray(state.base)) {
+//       if(!shallowEqual(state.proxy, state.base)) {
+//         markChanged(state);
+//       }
+//     } else if (Object.keys(state.proxy).length !== Object.keys(state.base).length) {
+//       markChanged(state);
+//     }
+//   });
+// };
+
+// const finalize = (proxy) => {
+//   if (proxy && proxy[PROXY_STATE]) {
+//     if (proxy.modified) {
+//       const state = proxy[PROXY_STATE];
+//       if (state.finalized) {
+//         return state.copy;
+//       }
+//       state.finalized = true;
+//       state.copy = shallowCopy(proxy);
+//       const base = state.base;
+//       each(state.copy, (key, value) => {
+//         if (value !== base[key]) {
+//           copy[key] = finalize(value);
+//         }
+//       });
+//       Object.freeze(copy);
+//     } else {
+//       return state.base;
+//     }
+//   }
+//   aa: each(proxy, (index, child) => {
+//     if (proxy && proxy[PROXY_STATE]) {
+//       proxy[index] = finalize(child);
+//     } else {
+//       aa(child);
+//     }
+//   });
+//   return proxy;
+// };
+
+// const prev = states;
+
+// try{
+//   // shallowCopy
+//   const proxy = Object.assign({}, base);
+
+//   // createPropertyProxy
+//   Object.keys(base).forEach((key) => {
+//     Object.defineProperty(proxy, key, cach[key] || {
+//       configurable: true,
+//       enumerable: true,
+//       get() {
+//           return get(this[PROXY_STATE], key)
+//       },
+//       set(value) {
+//           set(this[PROXY_STATE], key, value)
+//       }
+//     });
+//   });
+
+//   // createState
+//   const state = {
+//     modified: false,
+//     hasCopy: false,
+//     parent: undefined,
+//     base,
+//     proxy,
+//     copy: undefined,
+//     finished: false,
+//     finalizing: false,
+//     finalized: false,
+//   };
+
+//   // createHiddenProperty
+//   Object.defineProperty(proxy, PROXY_STATE, {
+//     value: state,
+//     enumerable: false,
+//     writable: true
+//   });
+//   states.push(state);
+
+//   const returnValue = producer.call(proxy, proxy);
+
+//   states.forEach(state => state.finalizing = true);
+
+//   markChanges();
+
+//   result = finalize(proxy);
+
+//   if (returnValue !== undefined && returnValue !== proxy) {
+//     if (proxy[PROXY_STATE].modified) {
+//       throw new Error('');
+//     }
+//     result = returnValue;
+//   }
+
+//   each(states, (i, state) => {
+//     state.finished = true;
+//   });
+//   return result;
+// } finally {
+//   states = prev;
+// }
+
+// produce(state, (draft) => {
+//   console.table(state);
+//   console.table(draft);
+// });
