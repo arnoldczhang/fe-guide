c3906cd9007ba25b456bff82ce51596d86b71c9f
arnold <zhangcheng17@meituan.com>
2018-03-14 17:24:36 +0800

    1.update；

--- src/js_related/my.js
+++ src/js_related/my.js
@@ -58,3 +58,21 @@ function isProxyable(value) {
   const proto = Object.getPrototypeOf(value);
   return proto === null || proto === Object.prototype;
 }
+
+
+
+
+// ajax
+readystate
+0：请求未初始化（还没有调用 open()）。
+1：请求已经建立，但是还没有发送（还没有调用 send()）。
+2：请求已发送，正在处理中（通常现在可以从响应中获取内容头）。
+3：请求在处理中；通常响应中已有部分数据可用了，但是服务器还没有完成响应的生成。
+4：响应已完成；您可以获取并使用服务器的响应了。
+
+
+
+
+
+
+
