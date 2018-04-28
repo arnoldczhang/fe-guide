47ac5c1aa87db3b45167c8bf61f82e8747e92b50
arnold <zhangcheng17@meituan.com>
2018-04-28 16:48:50 +0800

    1.update；

--- src/sicp/sicp.js
+++ src/sicp/sicp.js
@@ -1,3 +1,6 @@
+const esprima = require('esprima');
+const babel = require('babel-core');
+
 // 求最大公约数
 const gcd = (a, b) => {
   if (b > a) {
@@ -12,3 +15,65 @@ const gcd = (a, b) => {
 
 console.log(gcd(16, 28));
 console.log(gcd(206, 40));
+
+
+// 帕斯卡三角
+const paska = (line = 1, result = {}) => {
+  line = line || 1;
+  const preLine = result[line];
+
+  let length;
+  let nextLine;
+
+  if (preLine) {
+    length = preLine.length;
+    nextLine = new Array(length + 1);
+    for (let i = 0; i <= length; i += 1) {
+      nextLine[i] = i ? preLine[i - 1] + (preLine[i] || 0) : 1;
+    }
+    result[--line] = nextLine;
+  } else {
+    result[--line] = [1];
+  }
+
+  if (line) {
+    return paska(line, result);
+  }
+  return result;
+};
+
+console.log(paska(8));
+
+// ast
+const scripts = `
+ const a = {
+   props: {
+     bindingPropLis_: {
+       type: Array,
+       default() {
+         return [];
+       },
+     },
+   },
+   data() {
+     return {
+       slots: [],
+     };
+   },
+   mounted() {
+     const $slots = this.$slots;
+     const slots = {};
+     Object.keys($slots).forEach((key) => {
+       slots[key] = $slots[key];
+     });
+     this.slots = slots;
+     this.$on('hello', (text) => {
+       console.log(text);
+     });
+     console.log(this);
+   },
+ };
+`;
+
+// console.log(JSON.stringify(esprima.parseScript(scripts), null, '  '));
+// console.log(JSON.stringify(babel.transform(scripts), null, '  '));
