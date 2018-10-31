1b6f8ef7f0bc1821f8195103eab4a4bf778b171e
arnold <zhangcheng17@meituan.com>
2018-10-31 16:59:45 +0800

    1.update socket;

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,16 @@
 const http = require('http');
 const url = require('url');
+const crypto = require('crypto');
+
+const constants = {
+  BINARY_TYPES: ['nodebuffer', 'arraybuffer', 'fragments'],
+  GUID: '258EAFA5-E914-47DA-95CA-C5AB0DC85B11',
+  kStatusCode: Symbol('status-code'),
+  kWebSocket: Symbol('websocket'),
+  EMPTY_BUFFER: Buffer.alloc(0),
+  NOOP: () => {}
+};
+
 const WebsocketServer = new require('ws').Server;
 
 const handleUpgrade = (request, socket, header, wsServer) => {
@@ -37,13 +48,52 @@ server.on('request', (req, res) => {
 });
 
 server.on('upgrade', (req, socket, head) => {
-  const pathname = url.parse(req.url).pathname;
-  console.log('pathname', pathname);
-  handleUpgrade(req, socket, head, websocketServer);
+  // native websocket
+  if (!socket.readable || !socket.writable) return socket.destroy();
+  socket.on('error', socketOnError);
+  const key = crypto.createHash('sha1')
+    .update(req.headers['sec-websocket-key'] + constants.GUID, 'binary')
+    .digest('base64');
+
+  const headers = [
+    'HTTP/1.1 101 Switching Protocols',
+    'Upgrade: websocket',
+    'Connection: Upgrade',
+    `Sec-WebSocket-Accept: ${key}`
+  ];
+
+  let protocol = req.headers['sec-websocket-protocol'];
+
+  if (protocol) {
+    protocol = protocol.trim().split(/ *, */);
+    headers.push(`Sec-WebSocket-Protocol: ${protocol}`);
+  }
+  socket.emit('headers', headers, req);
+  socket.write(headers.concat('\r\n').join('\r\n'));
+  socket.removeListener('error', socketOnError);
+  socket.on('connection', (ws) => {
+    const str = 'hellloooooo world';
+    const offset = 2;
+    Buffer.allocUnsafe(str.length + offset);
+    socket.write(Buffer.from('hellloooooo world'));
+    socket.on('message', (message) => {
+      // socket.write('aaaa');
+      console.log('server: received: %s', message);
+    });
+  });
+
+  socket.emit('connection', socket, req);
+  // const pathname = url.parse(req.url).pathname;
+  // console.log('pathname', pathname);
+
+  // 原版websocket
+  // handleUpgrade(req, socket, head, websocketServer);
 });
 
 
-
+function socketOnError () {
+  this.destroy();
+}
 
 // const engine = require('engine.io');
 // const server = engine.listen(8999, {}, () => {
