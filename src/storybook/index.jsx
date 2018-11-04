0c9b71aae1c5e392c80f624b2055d3bff93478a5
zhangcheng17 <zhangcheng17@meituan.com>
2018-11-04 15:18:01 +0800

    1.update;

---
+++ src/storybook/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+import Starter from '../game/components/starter/Starter.tsx';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+  ), { notes: 'Here we use some emoji as the Button text. Doesn&apos;t it look nice?' })
+  .add('without props', () => <Starter />)
+  ; 
