f99d4a939551d7cc38cd9fe11ee2e3b3d71cb237
arnold <arnold.c.zhang@gmail.com>
2019-03-18 19:22:51 +0800

    update

--- src/babel/treeshaking-v1/treeshaking-search.js
+++ src/babel/treeshaking-v1/treeshaking-search.js
@@ -5,136 +5,131 @@ const uglifyJS = require('uglify-js');
 const fs = require('fs-extra');
 const path = require('path');
 const resolve = require('resolve');
+const say = console.log;
+const strify = (target = []) => target.toString();
 
-const map = {};
+const MAX = 300;
+let count = 1;
+
+const importMap = {};
+const exportMap = {};
+const visitedSet = new Set;
+
+const projectDir = path.join(__dirname, '../../waimai_wxapp/src');
+const nodeDir = path.join(__dirname, '../../waimai_wxapp/node_modules');
+const IMPORT = './test1/import.json';
+const OUTPORT = './test1/export.json';
+const defaultOutFileCb = (input) => {
+  say(`Compile file: ${input} SUCCESS`);
+};
+
+const transSetObjectToArray = (object = {}) =>
+  Object.keys(object).forEach(key =>
+    object[key] = Array.from(object[key])
+  );
 
 const compileFile = (filename, options = {}) => {
-  const filePath = resolve.sync(filename, __dirname);
-  const { minified } = options;
-  const input = fs.readFileSync(path.join(__dirname, filename), 'utf-8');
+  if (count >= MAX - 1) return;
+  const { basedir = projectDir } = options;
+  const filePath = resolve.sync(filename, { basedir });
+  if (visitedSet.has(filePath)) {
+    return;
+  }
+  visitedSet.add(filePath);
+  const fileDir = path.join(filePath, '../');
+  const input = fs.readFileSync(filePath, 'utf-8');
   const { ast } = transformSync(input, {
     filename,
     ast: true,
     code: false,
     sourceMap: true,
-    babelrc: false,
+    babelrc: true,
     configFile: false,
-    presets: [
-      ["@babel/env", !minified ? {
-        loose: false,
-        modules: false,
-      } : {
-        loose: false,
-      }],
-      // "minify",
-    ],
-    plugins: [
-      "minify-dead-code-elimination"
-    ],
   });
 
   traverse(ast, {
     Program: {
       enter() {
-        this.__funcs = {}; // 文件中声明的function
-        this.__alias = {}; // function对应的别名
-        this.__exports = [];
+        exportMap[filePath] = exportMap[filePath] || new Set;
+        this.__export = exportMap[filePath];
       },
       exit(path) {
-        const usedMember = map[filePath] || new Set;
-        Object.keys(this.__funcs).forEach((funcKey) => {
-          const paths = this.__funcs[funcKey];
-          const alias = this.__alias[funcKey];
-          if (alias) {
-            let used;
-            alias.forEach((alia) => {
-              if (used) return;
-              if (usedMember.has(alia)) {
-                used = true;
-              }
-            });
-
-            if (!used) {
-              paths.forEach(path => path.remove());
-              this.__funcs[funcKey] = null;
-            }
-          } else if (!usedMember.has(funcKey)) {
-            paths.forEach(path => path.remove());
-            this.__funcs[funcKey] = null;
-          }
-        });
-
-        this.__exports.forEach((specifiers) => {
-          specifiers.forEach((specifier, i) => {
-            const key = specifier.local.name;
-            if (!this.__funcs[key]) {
-              specifiers[i] = null;
-            }
-          });
-        });
+        const { callback = defaultOutFileCb } = options;
+        callback(filePath);
+        count += 1;
       }
     },
     ImportDeclaration(path) {
       const { node } = path;
       const source = path.get('source');
       const specifiers = path.get('specifiers');
-      const usedVariables = specifiers.map(specifier => specifier.get('imported').node.name);
-      const importPath = resolve.sync(source.node.value, __dirname);
-      map[importPath] = map[importPath] || new Set;
-      usedVariables.forEach(val => map[importPath].add(val));
+      const sourceName = source.node.value;
+      const importPath = resolve.sync(sourceName, { basedir: fileDir });
+      importMap[importPath] = importMap[importPath] || new Set;
+      specifiers.forEach((specifier) => {
+        const local = specifier.get('local').node;
+        const imported = specifier.get('imported');
+        if (imported && imported.node) {
+          importMap[importPath].add(imported.node.name);
+          compileFile(sourceName, {
+            basedir: /^\./.test(sourceName) ? fileDir : nodeDir,
+          });
+        }
+      });
+    },
+    ExportDefaultDeclaration(path) {
+      const exportNode = path.get('decleration').node;
+      this.__export.add(strify([exportNode ? exportNode.name : 'default']));
     },
     ExportNamedDeclaration(path) {
       const pathDeclaration = path.get('declaration');
       if (pathDeclaration && pathDeclaration.node) {
-        const declarations = pathDeclaration.get('declarations');
-        const exportNames = declarations.map(declare => declare.get('id').node.name);
-        if (!(map[filePath] || new Set).has(exportNames[0])) {
-          path.remove();
+        if (pathDeclaration.isVariableDeclaration()) {
+          const declarations = pathDeclaration.get('declarations');
+          if (declarations && declarations.length) {
+            const exportName = declarations.map(declare => declare.get('id').node.name)[0];
+            const exportValue = declarations.map(declare => declare.get('init'))[0];
+            if (exportValue.isIdentifier()) {
+              this.__export.add(strify([exportName, exportValue.node.name]));
+            } else if (exportValue.isFunction() || exportValue.isCallExpression()) {
+              this.__export.add(strify([exportName]));
+            }
+          }
+        } else if (pathDeclaration.isFunctionDeclaration()) {
+          const exportName = pathDeclaration.get('id').node.name;
+          this.__export.add(strify([exportName]));
         }
       } else {
         path.node.specifiers.forEach((specifier) => {
           const key = specifier.local.name;
           const alias = specifier.exported.name;
-          this.__alias[key] = this.__alias[key] || new Set;
-          this.__alias[key].add(alias);
+          this.__export.add(strify([alias, key]));
         });
-        this.__exports.push(path.node.specifiers);
-      }
-    },
-    FunctionDeclaration(path) {
-      const name = path.get('id').node.name;
-      if (name) {
-        this.__funcs[name] = this.__funcs[name] || [];
-        this.__funcs[name].push(path);
       }
     },
   });
-
-  let { code: babelCode } = babelGenerator(ast, {
-    minified,
-  });
-
-  if (minified) {
-    babelCode = uglifyJS.minify(babelCode, {
-      output: {},
-      compress: {
-        dead_code: true,
-        global_defs: {
-          ENV: 'tt',
-        }
-      },
-      mangle: {
-        toplevel: false,
-      },
-    }).code;
-  }
-  console.log('\n===========');
-  console.log(babelCode);
 };
 
-compileFile('./test.js', {
-  minified: false,
-});
-compileFile('./test-lib.js', {
-  minified: false,
-});
+const outputToFile = () => {
+  transSetObjectToArray(importMap);
+  transSetObjectToArray(exportMap);
+  fs.writeFile(IMPORT, JSON.stringify(importMap, null, 2));
+  say(`total: ${count}, success: ${count}`);
+  say(`write to ${IMPORT}`);
+  fs.writeFile(OUTPORT, JSON.stringify(exportMap, null, 2));
+  say(`write to ${OUTPORT}`);
+  say('DONE');
+};
+
+const analyzeImportFile = (filename = 'app.json') => {
+  const { pages } = JSON.parse(
+    fs.readFileSync(
+      path.join(projectDir, filename), 'utf-8'
+    )
+  );
+  pages.forEach(page => compileFile(`./${page}.js`));
+  compileFile('./app.js');
+  outputToFile();
+};
+
+analyzeImportFile();
