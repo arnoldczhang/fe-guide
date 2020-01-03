5c24dbc77ae329aa8b753c8712486cdb9e904d14
arnoldzhang <zhangcheng10682@hellobike.com>
2020-01-03 11:22:05 +0800

    update

---
+++ src/sql/const.ts
@@ -0,0 +1,21 @@
+const TYPE: ICO = {
+  eq: 'eq',
+  gt: 'gt',
+  gte: 'gte',
+  lt: 'lt',
+  lte: 'lte',
+  and: 'and',
+  divide: 'divide',
+  multiply: 'multiply',
+  add: 'add',
+  minus: 'minus',
+  max: 'max',
+  min: 'min',
+  avg: 'avg',
+  count: 'count',
+  toUInt32: 'toUInt32',
+  toUnixTimestamp: 'toUnixTimestamp',
+  wrap: 'wrap',
+};
+
+export default TYPE;
