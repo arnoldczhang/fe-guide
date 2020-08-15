4675cc46633f78810027cb3945e0531fc41cfbda
arnoldczhang <arnold.c.zhang@gmail.com>
2020-08-16 00:02:40 +0800

    update;

---
+++ src/algorithm/leetcode/二叉树-相同树.js
@@ -0,0 +1,25 @@
+/**
+ * 题目：
+ * 
+ * 二叉树-相同树
+ * 
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的
+ * 
+ */
+function isSameTree(p, q) {
+  const isSame = (pre, next) => {
+    if (pre === null && next === null) {
+        return true;
+    }
+
+    if (pre === null || next === null) {
+      return false;
+    }
+
+    if (pre.val !== next.val) {
+        return false;
+    }
+    return isSame(pre.left, next.left) && isSame(pre.right, next.right);
+  };
+  return isSame(p, q);
+};
