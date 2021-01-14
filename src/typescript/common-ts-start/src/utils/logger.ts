3a4036d42b2d86051884771c9e686429f82b26dd
arnoldzhang <arnold.c.zhang@gmail.com>
2021-01-14 22:06:00 +0800

    update

---
+++ src/typescript/common-ts-start/src/utils/logger.ts
@@ -0,0 +1,8 @@
+import * as chalk from 'chalk';
+
+export const success = (text: string) => console.log(chalk.green(text));
+
+export const error = (text: string) => {
+  console.log(chalk.red(text));
+  throw new Error(text);
+};
