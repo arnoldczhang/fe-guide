e04346afa7c2e05df6efb684f463ef4b10f1bb57
arnoldzhang <arnold.c.zhang@gmail.com>
2021-09-20 20:14:09 +0800

    update

--- src/vue/vue-smart-table/const.ts
+++ src/vue/vue-smart-table/const.ts
@@ -1,1 +1,13 @@
+// 标记el-form-item检测异常的初始化
 export const INITIAL_SYMBOL = Symbol('initial');
+// 标记scope-slot类型
+export const SCOPE_TYPE = Symbol('scope-type');
+// 标记slot="header"
+export const SLOT_HEADER = 'SlOt-HeAdEr';
+// 标记用helper里的ScopeWrapper包过的compName
+export const SCOPE_WRAPPER_NAME = 'ScOpE_wRaPpEr_NaMe';
+// el-table里默认的slotName
+export const tableDefaultSlots = [
+  'empty',
+  'header',
+];
