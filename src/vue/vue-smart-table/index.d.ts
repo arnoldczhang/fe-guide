9ff9b8f1cafcae9ebca12feaf7268c37b2ce317e
arnoldzhang <arnold.c.zhang@gmail.com>
2021-09-22 15:18:55 +0800

    update

--- src/vue/vue-smart-table/index.d.ts
+++ src/vue/vue-smart-table/index.d.ts
@@ -1,34 +1,43 @@
+import { Constructor } from 'vue/types/options';
+
+export interface VueRender {
+  render: (h: Vue.CreateElement, scope?: TableScope) => Vue.VNode | Vue.VNode[];
+}
+
 export interface CO<T = any> {
-    [key: string]: T;
+  [key: string]: T;
 }
 export interface CommonKey {
-    $props?: CO;
-    $listeners?: CO<Function>;
+  $props?: CO | Function;
+  $listeners?: CO<Function>;
 }
 
-export type Type = string | { render: (h: Vue.CreateElement, scope?: TableScope) => Vue.VNode | Vue.VNode[] };
+export type Type = string | symbol | { render: (h: Vue.CreateElement, scope?: TableScope) => Vue.VNode | Vue.VNode[] } | Constructor;
 
 export interface TableOption extends CommonKey {
-    key: string;
-    type?: Type;
-    // true: 纯自定义组件（不会包el-form-item）
-    pure?: boolean;
-    header?: CommonKey | string;
-    rules?: string | (Function | string)[];
-    children?: TableOption[] | Function;
+  key: string;
+  type?: Type;
+  // true: 纯自定义组件（不会包el-form-item）
+  pure?: boolean;
+  header?: CommonKey | string;
+  rules?: string | (Function | string)[];
+  children?: TableOption[] | Function;
 }
 
 export interface TableScope extends CommonKey {
-    row: CO;
-    column: CO;
-    $index: number;
-    error?: CO[];
-    validate?: Function;
+  row: CO;
+  column: CO;
+  $index: number;
+  event?: Event;
+  error?: CO[];
+  validate?: Function;
+  validateLine?: Function;
+  validateColumn?: Function;
 }
 
 export interface TableConfig {
-    table?: CommonKey;
-    pagination?: CommonKey;
-    column: (TableOption | string | TableOption[])[];
-    data: object[];
+  table?: CommonKey;
+  pagination?: CommonKey;
+  column: (TableOption | string | TableOption[])[];
+  data: object[];
 }
