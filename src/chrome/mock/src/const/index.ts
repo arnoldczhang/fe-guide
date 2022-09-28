1ed87cd7c69c93285b6f9299803599d04e066ed3
arnoldzhang <arnold.c.zhang@gmail.com>
2022-09-28 10:01:31 +0800

    update

---
+++ src/chrome/mock/src/const/index.ts
@@ -0,0 +1,13 @@
+export const STATUS = {
+  enable: true,
+  disable: false,
+};
+
+export const TYPE = {
+  object: 'object',
+  array: 'array',
+  null: 'null',
+  string: 'string',
+  number: 'number',
+  boolean: 'boolean',
+};
