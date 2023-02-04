2c44354f3897c0052b4fffda5aaec7487799765b
arnoldzhang <arnold.c.zhang@gmail.com>
2023-02-04 21:38:31 +0800

    update

--- src/algorithm/leetcode/回溯-排列组合all.js
+++ src/algorithm/leetcode/回溯-排列组合all.js
@@ -1,5 +1,10 @@
 /**
- * Q1：给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合
+ * 子集/全排列/组合总和
+ */
+
+/**
+ * Q1：子集（无重不可复选）
+ * - 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合
  */
 // const backtrace = (max = 1, count = 2, num = 1, result = [], cach = []) => {
 //   for (; num <= max; num += 1) {
@@ -28,7 +33,8 @@
 
 
 /**
- * Q2：返回无重复数组内所有元素的可能组合
+ * Q2：组合（无重不可复选）
+ * - 返回无重复数组内所有元素的可能组合
  */
 // const backtrace = (arr = [], index = 0, result = [], cach = []) => {
 //   const { length } = arr;
@@ -48,7 +54,8 @@
 
 
 /**
- * Q3：返回无重复、顺序排序的数组的全排列组合
+ * Q3：全排列（无重不可复选）
+ * - 返回无重复、顺序排序的数组的全排列组合
  */
 // const backtrace = (arr = [], result = [], cach = [], length = arr.length) => {
 //   for (let i = 0; i < arr.length; i += 1) {
@@ -77,8 +84,8 @@
 
 
 /**
- * Q4：返回有重复数组内所有元素的可能组合
- * 
+ * Q4：组合（有重不可复选）
+ * - 返回有重复数组内所有元素的可能组合
  * - 思路：排序完，相邻的重复元素不重复push
  */
 // const backtrace = (arr = [], result = [], start = 0, cach = []) => {
@@ -112,7 +119,8 @@
 
 
 /**
- * Q5：返回有重复数组内，元素和（不重复）为某个值的组合
+ * Q5：组合总和（有重不可复选）
+ * - 返回有重复数组内，元素和（不重复）为某个值的组合
  * 
  */
 // const backtrace = (arr = [], sum = 0, result = [], cach = [], index = 0, count = 0) => {
@@ -140,13 +148,14 @@
 
 
 /**
- * Q6：返回有重复的数组的全排列组合
+ * Q6：全排列（有重不可复选）
+ * - 返回有重复的数组的全排列组合
  */
 // const backtrace = (arr = [], result = [], cach = [], used = []) => {
 //   arr.sort((pre, next) => pre - next);
 //   for (let index = 0; index < arr.length; index += 1) {
 //     if (used.includes(index)) continue;
-//     // 相邻元素相同且已被用过，去重
+//     // 相邻元素相同且已被用过，去重（used也可以，但是!used剪枝更多）
 //     if (index > 0 && arr[index] === arr[index - 1] && !used.includes(index - 1)) {
 //       continue;
 //     }
@@ -167,7 +176,8 @@
 
 
 /**
- * Q7：返回无重复数组，元素和为目标值的所有组合（元素可重复）
+ * Q7：组合总和（无重可复选）
+ * - 返回无重复数组，元素和为目标值的所有组合（元素可重复）
  */
 // const backtrace = (arr = [], sum = 0, result = []) => {
 //   arr.sort((pre, next) => pre - next);
@@ -204,4 +214,40 @@
 // //   [ 1, 2, 3 ],
 // //   [ 2, 2, 2 ],
 // //   [ 3, 3 ]
+// // ]
+
+
+
+/**
+ * Q8：全排列（无重可复选）
+ * - 输入数组无重复元素，但每个元素可以被无限次使用
+ */
+// const backtrace = (arr = [], result = []) => {
+//   const { length } = arr;
+//   const iterator = (cach = []) => {
+//     for (let i = 0; i < length; i += 1) {
+//       const nextCach = cach.concat(arr[i]);
+//       if (nextCach.length === length) {
+//         result.push(nextCach);
+//         continue;
+//       }
+//       iterator(nextCach);
+//     }
+//   };
+//   iterator();
+//   return result;
+// };
+
+// // test
+// console.log(backtrace([1, 2, 3]));
+// // [
+// //   [ 1, 1, 1 ], [ 1, 1, 2 ], [ 1, 1, 3 ],
+// //   [ 1, 2, 1 ], [ 1, 2, 2 ], [ 1, 2, 3 ],
+// //   [ 1, 3, 1 ], [ 1, 3, 2 ], [ 1, 3, 3 ],
+// //   [ 2, 1, 1 ], [ 2, 1, 2 ], [ 2, 1, 3 ],
+// //   [ 2, 2, 1 ], [ 2, 2, 2 ], [ 2, 2, 3 ],
+// //   [ 2, 3, 1 ], [ 2, 3, 2 ], [ 2, 3, 3 ],
+// //   [ 3, 1, 1 ], [ 3, 1, 2 ], [ 3, 1, 3 ],
+// //   [ 3, 2, 1 ], [ 3, 2, 2 ], [ 3, 2, 3 ],
+// //   [ 3, 3, 1 ], [ 3, 3, 2 ], [ 3, 3, 3 ]
 // // ]
