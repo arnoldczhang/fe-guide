a354f9ad9c98ea068cf009a02a5e4e80a5ccf89c
arnoldzhang <arnold.c.zhang@gmail.com>
2023-11-26 20:42:03 +0800

    update

--- src/fe-interview/src/zookeeper.js
+++ src/fe-interview/src/zookeeper.js
@@ -15,98 +15,112 @@
  *  > getValue(path)
  *  > setValue(path, value)
  */
-class ZooKeeper {
+class Zookeeper {
   constructor() {
-    this.root = this.createRoot();
-  }
-
-  createRoot() {
-    return {
+    this.root = {
       path: '/',
-      value: null,
-      root: true,
       children: [],
-      listener: {},
+      value: null,
     };
-  }
-
-  createNode(path, value) {
-   return {
-    path,
-    value,
-    root: false,
-    children: [],
-    listener: {},
-   } 
-  }
-
-  iterateInsert(list = [], node = this.root) {
-    if (!list.length) return node;
-    const [first, ...other] = list;
-    const existed = node.children.find(({ path }) => path === first);
-
-    if (existed) {
-      return this.iterateInsert(other, existed);
-    }
-
-    const newNode = this.createNode(first);
-    node.children.push(newNode);
-    return this.iterateInsert(other, newNode);
-  }
-
-  checkExisted(list, node = this.root) {
-    const [first, ...other] = list;
-    const existed = node.children.find(({ path }) => path === first);
-    if (existed) {
-      if (other.length) {
-        return this.checkExisted(other, existed);
-      }
-      return [node, existed];
-    }
-    return -1;
-  }
-
-  create(path) {
-    const pathList = path.split('/').filter(v => v);
-    return this.iterateInsert(pathList);
-  }
-
-  delete(path) {
-    const pathList = path.split('/').filter(v => v);
-    const existed = this.checkExisted(pathList);
-    if (existed !== -1) {
-      const [parent, child] = existed;
-      parent.children.splice(parent.children.indexOf(child), 1);
-      return existed;
-    }
-    return false;
-  }
-
-  getValue(path) {
-    const pathList = path.split('/').filter(v => v);
-    const existed = this.checkExisted(pathList);
-    if (existed) return existed[1].value;
-    throw new Error('not existed');
-  }
-  
-  setValue(path, value) {
-    const node = this.create(path);
-    node.value = value;
   }
 
   getRoot() {
     return this.root;
   }
+
+  getChildPath(path) {
+    return path.split(/\//).filter(Boolean);
+  }
+
+  getPathList(path) {
+    return Array.isArray(path) ? path : this.getChildPath(path);
+  }
+
+  create(path, node = this.root) {
+    const pathList = this.getPathList(path);
+    while (pathList.length) {
+      const tempPath = pathList.shift();
+      let matched = node.children.find(({ path }) => path === tempPath);
+      if (matched) {
+        node = matched;
+        continue;
+      }
+
+      matched = {
+        path: tempPath,
+        parent: node,
+        children: [],
+        value: null,
+      };
+
+      if (pathList.length) {
+        this.create(pathList, matched);
+      }
+      node.children.push(matched);
+      break;
+    }
+  }
+
+  delete(path, node = this.root) {
+    const pathList = this.getPathList(path);
+    let index = -1;
+    while (pathList.length) {
+      const tempPath = pathList.shift();
+      let matched = node.children.find(({ path }) => path === tempPath);
+      if (matched) {
+        index = node.children.findIndex(value => value === matched);
+        node = matched;
+        continue;
+      }
+
+      throw new Error(`${path} 不存在`);
+    }
+    node.parent.children.splice(index, 1);
+    return node;
+  }
+
+  get(path, node = this.root) {
+    const pathList = this.getPathList(path);
+    while (pathList.length) {
+      const tempPath = pathList.shift();
+      let matched = node.children.find(({ path }) => path === tempPath);
+      if (matched) {
+        node = matched;
+        continue;
+      }
+
+      throw new Error(`${path} 不存在`);
+    }
+    return node;
+  }
+
+  getValue(path) {
+    return this.get(path).value;
+  }
+
+  setValue(path, value) {
+    const node = this.get(path);
+    node.value = value;
+  }
 }
 
 // test
-const zookeeper = new ZooKeeper();
-zookeeper.create('/app/p_1/p_1_1');
-zookeeper.create('/app/p_1/p_1_2');
-zookeeper.delete('/app/p_1/p_1_2');
-console.log(zookeeper.getValue('/app/p_1/p_1_1'));
-zookeeper.setValue('/app/p_1/p_1_1', 100);
-zookeeper.delete('/app/p_2');
-zookeeper.delete('/app/p_2');
-console.log(zookeeper.getValue('/app/p_1/p_1_1'));
-console.log(JSON.stringify(zookeeper.getRoot(), null, 4));
+const zk = new Zookeeper();
+
+zk.create('/app/p_1/p_1_1');
+zk.delete('/app/p_1/p_1_1');
+zk.create('/app/p_1/p_1_1');
+zk.delete('/app/p_1/p_1_1');
+zk.create('/app/p_1/p_1_1');
+zk.create('/app/p_1/p_1_2');
+zk.delete('/app/p_1/p_1_2');
+console.log(zk.getValue('/app/p_1/p_1_1'));
+zk.setValue('/app/p_1/p_1_1', 200);
+zk.create('/app/p_2');
+zk.setValue('/app/p_2', 100);
+console.log(zk.getValue('/app/p_2'));
+zk.delete('/app/p_2');
+console.log(zk.getValue('/app/p_1/p_1_1'));
+zk.setValue('/app/p_1/p_1_1', 300);
+console.log(zk.getValue('/app/p_1/p_1_1'));
+zk.delete('/app/p_2');
