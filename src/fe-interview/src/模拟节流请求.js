5203cfe109e6f4999c310cfadae9b5c521931843
arnoldzhang <arnold.c.zhang@gmail.com>
2023-12-01 10:11:50 +0800

    update

--- src/fe-interview/src/模拟节流请求.js
+++ src/fe-interview/src/模拟节流请求.js
@@ -10,36 +10,69 @@
  * 
  */
 class Rolling {
-  constructor(q, max) {
-    this.q = q;
-    this.max = max;
-    this.pending = 0;
-    this.on = true;
-  }
-  
-  start() {
-      if (!this.q.length) {
-        return this.stop();
-      }
-      while (this.pending <= this.max) {
-        const reqUrl = this.q.shift();
-        this.request(reqUrl);
-        this.pending += 1;
-      }
+  constructor(queue = [], parallel = 5, callback) {
+    this.queue = queue;
+    this.parallel = parallel;
+    this.currentParallel = 0;
+    this.pollingQueue = [];
+    this.runnable = true;
+    this.callback = callback;
   }
 
-  
-  stop() {
-    this.on = false;
-  }
-  
-  request(url) {
-    fetch(url).then((res) => {
-      this.pending -= 1;
-      this.start();
-    }).catch((err) => {
-      this.pending -= 1;
+  async request(url = '') {
+    if (typeof this.callback === 'function') {
+      try {
+        const result = await this.callback(url);
+        console.log('success', result);
+      } catch(err) {
+        this.queue.unshift(url);
+      } finally {
+        this.currentParallel--;
+        this.start();
+        return;
+      }
+    }
+    return new Promise((resolve, reject) => {
+      Math.random() > 0.3 ? resolve(url) : reject(url);
+    }).then((res) => {
+      console.log('success', url);
+    }, (err) => {
+      console.log('fail', url);
+      this.queue.unshift(url);
+    }).finally(() => {
+      this.currentParallel--;
       this.start();
     });
   }
+
+  start() {
+    if (!this.runnable) return;
+    if (!this.queue.length) return;
+    while (this.queue.length) {
+      if (this.currentParallel >= this.parallel) return;
+      const url = this.queue.shift();
+      this.request(url);
+      this.currentParallel += 1;
+    }
+  }
+
+  stop() {
+    this.runnable = false;
+  }
+
+  restart() {
+    this.runnable = true;
+    this.start();
+  }
 }
+
+// test
+const queue = new Array(100).fill().map(() => Math.random());
+const max = 5; // 并行请求数
+const roll = new Rolling(queue, max, async (res) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 3333);
+  });
+  return res;
+});
+roll.start()
