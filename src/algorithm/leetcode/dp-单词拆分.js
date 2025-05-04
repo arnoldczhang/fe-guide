e5f0397d63ffaa618f12ab140a2edbe57c50c3d7
arnoldzhang <arnold.c.zhang@gmail.com>
2025-05-04 22:24:30 +0800

    update

---
+++ src/algorithm/leetcode/dp-单词拆分.js
@@ -0,0 +1,47 @@
+/**
+ * 139. 单词拆分
+ * 
+ * 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。如果可以利用字典中出现的一个或多个单词拼接出 s 则返回 true。
+ * 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+ * 
+ * 
+ * 示例 1：
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+ * 
+ * 示例 2：
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+ * 注意，你可以重复使用字典中的单词。
+ * 
+ * 示例 3：
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ * 
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ * 
+ * 提示：
+ * 1. 除了空字符串，默认都是未匹配false
+ * 2. 分段匹配，即当前段是否匹配，还取决于上一段是否匹配
+ * 
+ */
+var wordBreak = function(s, wordDict) {
+  const dp = Array(s.length + 1).fill(false);
+  // 空字符串，默认是能匹配的
+  dp[0] = true;
+  for (let i = 1; i <= s.length; i += 1) {
+    for (let j = 0; j < wordDict.length; j += 1) {
+      const dict = wordDict[j];
+      if (i < dict.length) continue;
+      // 当前段和之前段都匹配，才是真匹配
+      if (s.slice(i - dict.length, i) === dict && dp[i - dict.length]) {
+        dp[i] = true;
+      }
+    }
+  }
+  return dp[s.length];
+};
--- src/algorithm/leetcode/dp-最长递增子序列长度.js
+++ src/algorithm/leetcode/dp-最长递增子序列长度.js
@@ -1,30 +1,31 @@
 /**
- * 题目：
+ * 300. 最长递增子序列
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
  * 
- * dp-最长递增子序列长度
+ *  
+ * 示例 1：
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
  * 
- * 输入: [10,9,2,5,3,7,101,18]
+ * 示例 2：
+ * 输入：nums = [0,1,0,3,2,3]
+ * 输出：4
  * 
- * 输出: [2,3,7,101]
- * 
- * 题解：
- * - 子串是连续的，子序列可以不连续
- * 
+ * 示例 3：
+ * 输入：nums = [7,7,7,7,7,7,7]
+ * 输出：1
  */
-
- // dp-table解
-function findLIS(nums) {
-  const { length } = nums;
-  const dp = Array.from({ length }).fill(1);
-  for (let i = 0; i < length; i += 1) {
+var lengthOfLIS = function(nums) {
+  const dp = Array(nums.length + 1).fill(1);
+  for (let i = 0; i < nums.length; i += 1) {
     for (let j = 0; j < i; j += 1) {
-      if (nums[j] < nums[i]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-      }
+      if (nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
     }
   }
   return Math.max(...dp);
-}
+};
 
 // [二分查找解法](https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247484498&idx=1&sn=df58ef249c457dd50ea632f7c2e6e761&chksm=9bd7fa5aaca0734c29bcf7979146359f63f521e3060c2acbf57a4992c887aeebe2a9e4bd8a89&scene=21#wechat_redirect)
 function findLIS2(nums) {
